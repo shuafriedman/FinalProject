b10608c4576f2bfea8dbbe422c074961709bc459
shuafriedman <sfriedman291@gmail.com>
2024-04-07 18:28:36 +0000

    implemented streaming

--- wav2vec_finetune/wav2vec2bert/wav2vec2bert-finetune.py
+++ wav2vec_finetune/wav2vec2bert/wav2vec2bert-finetune.py
@@ -9,7 +9,7 @@ from transformers import SeamlessM4TFeatureExtractor
 from transformers import Wav2Vec2BertProcessor, Wav2Vec2BertForCTC, AutoProcessor
 from transformers import Trainer, TrainingArguments
 #import load_metric
-from datasets import load_metric, DatasetDict, load_from_disk
+from datasets import load_metric, DatasetDict, load_from_disk, load_dataset
 from config import *
 import torch
 import numpy as np
@@ -62,19 +62,26 @@ def compute_metrics_custom(wer_metric, processor):
 def load_dataset_from_disk():
     dataset_name = DATASETS[0]['local_path']
     suffix = "-filtered" if FILTER_LONG_SAMPLES else None
-    dataset= load_from_disk(f"{DATA_FOLDER_PATH}/{dataset_name}{suffix}")
+    if FROM_HUB:
+        dataset=load_dataset(f"Shuaf98/{dataset_name}{suffix}", streaming=True)
+        train_samples_len=None
+    else:
+        dataset= load_from_disk(f"{DATA_FOLDER_PATH}/{dataset_name}{suffix}")
+        train_samples_len=dataset['train'].num_rows
+        for name in dataset.keys():
+            dataset[name] = dataset[name].to_iterable_dataset()
     if DRY_RUN:
-        small_train_subset = dataset['train'].select(range(32))
-        small_test_subset = dataset['test'].select(range(32)) 
+        small_train_subset = dataset['train'].select(range(128))
+        small_test_subset = dataset['test'].select(range(128)) 
         dataset = DatasetDict({"train": small_train_subset, "test": small_test_subset})
-    return dataset
+    return dataset, train_samples_len
 
 def main():
     print("Loading tokenizer")
     print("Loading Processor")
     processor = Wav2Vec2BertProcessor.from_pretrained(LOCAL_MODEL_PATH if DOWNLOAD_MODEL_LOCALLY else None) #TODO
     print("Loading Dataset")
-    dataset = load_dataset_from_disk()
+    dataset, train_samples_len = load_dataset_from_disk()
     print("Mapping Dataset Processor to Dataset")
 
     
@@ -93,21 +100,29 @@ def main():
         ctc_loss_reduction="mean",
         add_adapter=True,
         pad_token_id=processor.tokenizer.pad_token_id,
-        vocab_size=len(processor.tokenizer)
+        vocab_size=len(processor.tokenizer),
     )
     print("Checking for cuda")
     fp16= True if torch.cuda.is_available() else False
     print("Getting Training Args")
+    batch_size = 4 if not DRY_RUN else 1
+    gradient_accumulation = 8 if not DRY_RUN else 2
+    num_epochs = 1 if not DRY_RUN else 1
+    if train_samples_len:
+        max_steps= num_epochs * train_samples_len / batch_size / gradient_accumulation
+    else:
+        max_steps=None
     training_args = TrainingArguments(
         output_dir= './',
-        group_by_length=True,
-        per_device_train_batch_size= 4 if not DRY_RUN else 1,
-        gradient_accumulation_steps=8 if not DRY_RUN else 2,
+        # group_by_length=True,
+        per_device_train_batch_size= batch_size,
+        gradient_accumulation_steps=gradient_accumulation,
         evaluation_strategy="steps",
-        num_train_epochs=1 if not DRY_RUN else 1,
+        num_train_epochs=num_epochs,
         gradient_checkpointing=True,
         fp16=fp16,
         save_steps=600,
+        max_steps = max_steps,
         eval_steps=300 if not DRY_RUN else 8,
         logging_steps=300,
         learning_rate=5e-5,
@@ -129,6 +144,25 @@ def main():
     trainer.train()
     print("Saving Model")
     trainer.save_model(FINETUNED_MODEL_PATH)
+# from accelerate import Accelerator
+# from torch.utils.data.dataloader import DataLoader
+
+# dataloader = DataLoader(ds, batch_size=training_args.per_device_train_batch_size)
+
+# if training_args.gradient_checkpointing:
+#     model.gradient_checkpointing_enable()
+
+# accelerator = Accelerator(fp16=training_args.fp16)
+# model, optimizer, dataloader = accelerator.prepare(model, adam_bnb_optim, dataloader)
+
+# model.train()
+# for step, batch in enumerate(dataloader, start=1):
+#     loss = model(**batch).loss
+#     loss = loss / training_args.gradient_accumulation_steps
+#     accelerator.backward(loss)
+#     if step % training_args.gradient_accumulation_steps == 0:
+#         optimizer.step()
+#         optimizer.zero_grad()
 if __name__=='__main__':
    main()
 
